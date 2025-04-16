6d4d4e6b1cfc9e051aaa313918bc70980d40db40
ashunair <ashwathynair1998@gmail.com>
2025-04-15 22:23:38 -0400

    Initial code commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// const express =  require('express');
+import express  from 'express';
+// import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from './config/db.js';
+
+// dotenv.config();
+const app = express();
+
+const PORT = ENV_VARS.PORT;
+
+app.use(express.json()); // will allow us to parse  req.body
+// console.log("MONGO_URI:" process.env.MONGO_URI);
+//  Application Route 
+
+app.use("/api/v1/auth", authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log('Server started at http://localhost:'+ PORT);
+    connectDB();
+});
