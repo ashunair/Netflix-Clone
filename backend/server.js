b7bd5a2217dbe1c404d439f8087f2dcb3d16b497
ashunair <ashwathynair1998@gmail.com>
2025-04-28 20:51:36 -0400

    code integration and generating JWT

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express  from 'express';
 // import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes  from "./routes/movie.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from './config/db.js';
 
@@ -16,10 +17,11 @@ app.use(express.json()); // will allow us to parse  req.body
 //  Application Route 
 
 app.use("/api/v1/auth", authRoutes);
-
+app.use("/api/v1/movie", movieRoutes);
 
 
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:'+ PORT);
     connectDB();
 });
+
