07b475882c646d04385126eaca95a4022f8af434
ashunair <ashwathynair1998@gmail.com>
2025-05-11 14:37:23 -0400

    middleware backend development to authenticate routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 // const express =  require('express');
 import express  from 'express';
+import cookieParser from "cookie-parser";
 // import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes  from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from './config/db.js';
+import { protectRoute } from "./middleware/protectRoute.js";
 
 // dotenv.config();
 const app = express();
@@ -15,12 +19,13 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse  req.body
-// console.log("MONGO_URI:" process.env.MONGO_URI);
-//  Application Route 
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
+
 
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:'+ PORT);
