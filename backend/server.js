cc640cd32390de10378c3adda2258e98b3150736
ashunair <ashwathynair1998@gmail.com>
2025-05-11 14:14:29 -0400

    tv routes and controllers backend development

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express  from 'express';
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes  from "./routes/movie.route.js";
-import movieRoutes  from "./routes/tv.route.js";
+import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from './config/db.js';
