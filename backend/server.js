4d4a9fbd02cf71f47cccec1ad4e95184fcd286e5
ashunair <ashwathynair1998@gmail.com>
2025-05-20 10:41:55 -0400

    Code package changes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
-// const express =  require('express');
-import express  from 'express';
+import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
-// import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
-import movieRoutes  from "./routes/movie.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 
-
 import { ENV_VARS } from "./config/envVars.js";
-import { connectDB } from './config/db.js';
+import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
-// dotenv.config();
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json()); // will allow us to parse  req.body
+app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
@@ -37,7 +33,6 @@ if (ENV_VARS.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-    console.log('Server started at http://localhost:'+ PORT);
-    connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
 });
-
